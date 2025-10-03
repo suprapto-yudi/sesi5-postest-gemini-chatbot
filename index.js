0ef486a226f058c33e50bd1667631736d73a4961
Eas <me@eas.web.id>
2025-10-03 20:59:44 +0700

    index: improve /chat endpoint

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const ai = new GoogleGenAI({});
 app.use(cors());
 // app.use(multer());
 app.use(express.json());
+app.use(express.static('public'));
 
 // 3. inisialisasi endpoint
 // [HTTP method: GET, POST, PUT, PATCH, DELETE]
@@ -32,30 +33,75 @@ app.post(
   '/chat', // http://localhost:[PORT]/chat
   async (req, res) => {
     const { body } = req;
-    const { prompt } = body;
+    const { conversation } = body;
+
+    // body
+    // {
+    //   conversation: [
+    //     { role: '' ( 'user' | 'model' ), text: '' }
+    //   ] // kita cek role sama text-nya ada atau nggak
+    // }
 
     // guard clause -- satpam
-    if (!prompt || typeof prompt !== 'string') {
+    if (!conversation || !Array.isArray(conversation)) {
       res.status(400).json({
-        message: "Prompt harus diisi dan berupa string!",
+        message: "Percakapan harus valid!",
         data: null,
         success: false
       });
       return;
     }
 
+    // guard clause #2 -- satpam ketat!
+    const conversationIsValid = conversation.every((message) => {
+      // kondisi pertama -- message harus truthy
+      if (!message) return false;
+
+      // kondisi kedua -- message harus berupa object, namun bukan array!
+      if (typeof message !== 'object' || Array.isArray(message)) return false;
+
+      // kondisi ketiga -- message harus berisi hanya role dan text
+      const keys = Object.keys(message);
+      const keyLengthIsValid = keys.length === 2;
+      const keyContainsValidName = keys.every(key => ['role', 'text'].includes(key));
+
+      if (!keyLengthIsValid || !keyContainsValidName) return false;
+
+      // kondisi keempat
+      // -- role harus berupa 'user' | 'model'
+      // -- text harus berupa string
+
+      const { role, text } = message;
+      const roleIsValid = ['user', 'model'].includes(role);
+      const textIsValid = typeof text === 'string';
+
+      if (!roleIsValid || !textIsValid) return false;
+
+      // selebihnya...
+
+      return true;
+    });
+
+    if (!conversationIsValid) {
+      res.status(400).json({
+        message: "Percakapan harus valid!",
+        data: null,
+        success: false
+      });
+      return;
+    }
+
+    const contents = conversation.map(({ role, text }) => ({
+      role,
+      parts: [{ text }]
+    }));
+
     // dagingnya (A5 wagyu nih)
     try {
       // 3rd party API -- Google AI
       const aiResponse = await ai.models.generateContent({
         model: 'gemini-2.5-flash',
-        contents: [
-          {
-            parts: [
-              { text: prompt }
-            ]
-          }
-        ]
+        contents
       });
 
       res.status(200).json({
